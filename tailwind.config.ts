b53cca412c1f24464359ae5f7503a9f3e989ab01
dominique <courregesdominique@gmail.com>
2025-01-10 17:35:41 -0300

    EstilosGlobales

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,19 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "black-dark": "#0d0d0d",
+        "black-light": "#1a1a1a",
+        "white-ivory": "#fdf6e3",
+        "white-basic": "#f8f8f8",
+        "gray-ash": "#b0b0b0",
+        "gold-soft": "#d4af37",
+        "gold-dark": "#92781f",
+        "green-olive": "#9cb380",
+        "green-dark": "#7c985a",
+      },
+      fontFamily: {
+        primary: ["var(--primary-font)", "serif"],
+        secondary: ["var(--secondary-font)", "sans-serif"],
       },
     },
   },
