6e6235289fddc992dc798a34d5550c97b03f3680
Mexposto <macarenaexposto@gmail.com>
2025-01-29 15:48:47 -0300

    nextconfig

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,10 @@
 import ClientProfile from "../../components/ClientProfile/ClientProfile";
-import ProtectedRoute from "@/components/ProtectedRoute/ProtectedRoute";
 
 const DashboardPage = () => {
   return (
-    <ProtectedRoute roles={["admin", "caretaker", "customer"]}>
-      <div>
-        <ClientProfile />
-      </div>
-    </ProtectedRoute>
+    <div>
+      <ClientProfile />
+    </div>
   );
 };
 
