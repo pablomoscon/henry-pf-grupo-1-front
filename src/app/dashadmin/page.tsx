fdd070d23cd43a5c16fcc672c03a91f4c2a7492a
Mexposto <macarenaexposto@gmail.com>
2025-01-31 18:24:02 -0300

    funcionalidad en dashboard de aadmin para crear, editar, listar, y eliminar caretakers

--- src/app/dashadmin/page.tsx
+++ src/app/dashadmin/page.tsx
@@ -1,9 +1,11 @@
 import ProtectedRoute from "@/components/ProtectedRoute/ProtectedRoute";
 import SuitesForm from "@/components/SuitesForm/SuitesForm";
+import CaretakerManager from "@/components/CaretakerManager/CaretakerManager";
 const DashAdmin = () => {
   return (
     <ProtectedRoute>
       <div>
+        <CaretakerManager />
         <SuitesForm />
       </div>
     </ProtectedRoute>
