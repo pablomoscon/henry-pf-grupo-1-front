eabab05c756d904ad3a64b460925ec4c7c61fc21
pablomos <mosconpablo@gmail.com>
2025-01-31 17:19:18 -0300

    add cancel payment

---
+++ src/app/payment/page.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import PaymentCancel from '@/components/PaymentCancel/PaymentCancel';
+import PaymentSuccess from '@/components/PaymentSuccess.tsx/PaymentSuccess';
+import Link from 'next/link';
+import { useSearchParams } from 'next/navigation';
+
+function Page() {
+  const searchParams = useSearchParams();
+  const sessionId = searchParams.get('sessionId');
+  const status = searchParams.get('status');
+
+  return (
+    <div className='text-3xl font-bold text-center py-10 h-screen flex items-center justify-center'>
+      <div>
+        {status === 'succeeded' ? (
+          <PaymentSuccess sessionId={sessionId} status={status} />
+        ) : status === 'canceled' ? (
+          <PaymentCancel sessionId={sessionId} status={status} />
+        ) : (
+          <p>No valid status specified.</p>
+        )}
+        <Link href='/' className='text-blue-500 block mt-4'>
+          Back to The Fancy Box
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Page;
