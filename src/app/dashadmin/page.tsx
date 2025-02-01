44a2990ed34c9962c78b8a08ae1fe085d1eab807
Mexposto <macarenaexposto@gmail.com>
2025-01-31 21:52:49 -0300

    userManager para dashboard de admin

--- src/app/dashadmin/page.tsx
+++ src/app/dashadmin/page.tsx
@@ -1,10 +1,13 @@
 import ProtectedRoute from "@/components/ProtectedRoute/ProtectedRoute";
 import SuitesForm from "@/components/SuitesForm/SuitesForm";
 import CaretakerManager from "@/components/CaretakerManager/CaretakerManager";
+//import UserManager from "@/components/UserManager/UserManager";
+
 const DashAdmin = () => {
   return (
     <ProtectedRoute>
       <div>
+        {/* <UserManager /> */}
         <CaretakerManager />
         <SuitesForm />
       </div>
