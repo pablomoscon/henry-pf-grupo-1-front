4dfa6eeb913acfe7e60f65f4e21bbb43a4bfe782
dominique <courregesdominique@gmail.com>
2025-02-06 09:52:18 -0300

    ajustes13

--- envs.ts
+++ envs.ts
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-export const API_URL: string = process.env.API_URL || "http://localhost:3001/";
+export const API_URL: string =
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/";
