970bcd90cb8996b46b3bc1559d3d8c37e272083b
Mexposto <macarenaexposto@gmail.com>
2025-02-07 18:28:22 -0300

    loading caretaker

--- src/components/CaretakerDash/CaretakerDash.tsx
+++ src/components/CaretakerDash/CaretakerDash.tsx
@@ -5,19 +5,21 @@ import { UserContext } from "@/contexts/userContext";
 import { getUserBooks } from "@/services/bookService";
 import CardBook from "../CardBook/CardBook";
 import { IBookCaretaker } from "@/interfaces/IBook";
-
+import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
 const CaretakerDash = () => {
   const { user } = useContext(UserContext);
   const userData = user?.response?.user;
   const token = user?.response.token;
 
   const [books, setBooks] = useState<IBookCaretaker[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchBooks = async () => {
       if (!userData?.id) return;
       const reservations = await getUserBooks(userData.id, token);
       setBooks(reservations);
+      setIsLoading(false);
     };
 
     fetchBooks();
@@ -27,19 +29,27 @@ const CaretakerDash = () => {
 
   return (
     <div className="flex flex-col items-center min-h-screen pt-24 pb-12 px-4">
-      <h1 className="text-4xl text-center text-gold-soft pb-8">
-        Welcome {userData?.name}
-      </h1>
+      {isLoading ? (
+        <LoadingSpinner />
+      ) : (
+        <>
+          <h1 className="text-4xl text-center text-gold-soft pb-8">
+            Welcome {userData?.name}
+          </h1>
 
-      <div className="bg-black-light border border-white-basic rounded-lg p-6 w-full max-w-4xl">
-        <h2 className="text-2xl text-gold-soft mb-4">My Guests</h2>
+          <div className="bg-black-light border border-white-basic rounded-lg p-6 w-full max-w-4xl">
+            <h2 className="text-2xl text-gold-soft mb-4">My Guests</h2>
 
-        {books.length > 0 ? (
-          books.map((book) => <CardBook key={book.userId} book={book} />)
-        ) : (
-          <p className="text-white-basic text-center">No reservations yet.</p>
-        )}
-      </div>
+            {books.length > 0 ? (
+              books.map((book) => <CardBook key={book.userId} book={book} />)
+            ) : (
+              <p className="text-white-basic text-center">
+                No reservations yet.
+              </p>
+            )}
+          </div>
+        </>
+      )}
     </div>
   );
 };
