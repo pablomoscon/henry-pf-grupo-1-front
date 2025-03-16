950ffbbe269f150171a512f97dee349143e34fcb
pablomos <mosconpablo@gmail.com>
2025-03-15 21:08:24 -0300

    edit chat-spinner

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -48,6 +48,14 @@ const Chat = () => {
     setNewMessage('');
   };
 
+  if (loading) {
+    return (
+      <div className='h-screen flex items-center justify-center bg-black-dark'>
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
   return (
     <div className='h-[calc(100vh-96px)] flex items-center justify-center bg-black-dark'>
       <div className='w-full max-w-4xl h-[90%] mx-2 bg-black-dark rounded-lg shadow-lg border border-gray-700 flex flex-col'>
@@ -55,80 +63,70 @@ const Chat = () => {
           Chat
         </h2>
 
+        {/* Mostrar mensaje de error si existe */}
         {errorMessage && (
           <div className='bg-red-600 text-white p-2 text-center rounded-md mx-4'>
             {errorMessage}
           </div>
         )}
 
-        {/* Mostrar spinner si está cargando */}
-        {loading ? (
-          <div className='flex-1 flex justify-center items-center'>
-           <LoadingSpinner/>
-          </div>
-        ) : (
-          <>
-            {/* Contenedor de mensajes */}
-            <div
-              ref={messagesContainerRef}
-              className='flex-1 overflow-y-auto m-4 p-4 space-y-4'
-              style={{
-                maxHeight: 'calc(100% - 100px)',
-                scrollBehavior: 'smooth',
-              }}
-            >
-              {messages.map((message, index) => {
-                const isSender =
-                  message.senderName === (user?.response?.user?.name || '');
+        {/* Contenedor de mensajes con scroll solo dentro y smooth scroll */}
+        <div
+          ref={messagesContainerRef}
+          className='flex-1 overflow-y-auto m-4 p-4 space-y-4'
+          style={{
+            maxHeight: 'calc(100% - 100px)',
+            scrollBehavior: 'smooth',
+          }}
+        >
+          {messages.map((message, index) => {
+            const isSender =
+              message.senderName === (user?.response?.user?.name || '');
 
-                return (
-                  <div
-                    key={index}
-                    className={`flex ${
-                      isSender ? 'justify-end' : 'justify-start'
-                    }`}
-                  >
-                    <div
-                      className={`min-w-[20%] rounded-lg p-3 me-2 ms-2 ${
-                        isSender
-                          ? 'bg-gold-soft text-black-dark'
-                          : 'bg-gray-700 text-white-basic'
-                      }`}
-                    >
-                      <p className='text-m font-bold'>{message.senderName}</p>
-                      <p>{message.body}</p>
-                      <p className='text-sm opacity-70'>
-                        {message.timestamp
-                          ? new Date(message.timestamp).toLocaleTimeString()
-                          : 'Invalid Time'}
-                      </p>
-                    </div>
-                  </div>
-                );
-              })}
-            </div>
-
-            {/* Input para enviar mensajes */}
-            <div className='p-4 border-t border-gray-700 bg-black-dark mt-auto'>
-              <div className='flex gap-2'>
-                <input
-                  type='text'
-                  value={newMessage}
-                  onChange={(e) => setNewMessage(e.target.value)}
-                  onKeyPress={(e) => e.key === 'Enter' && handleSend()}
-                  className='flex-1 bg-gray-700 text-white-basic rounded-lg px-4 py-2'
-                  placeholder='Type your message...'
-                />
-                <button
-                  onClick={handleSend}
-                  className='bg-gold-soft text-black-dark px-4 py-2 rounded-lg hover:bg-gold-dark transition-colors'
+            return (
+              <div
+                key={index}
+                className={`flex ${isSender ? 'justify-end' : 'justify-start'}`}
+              >
+                <div
+                  className={`min-w-[20%] rounded-lg p-3 me-2 ms-2 ${
+                    isSender
+                      ? 'bg-gold-soft text-black-dark' // Color para el sender
+                      : 'bg-gray-700 text-white-basic' // Color para el receiver
+                  }`}
                 >
-                  Send
-                </button>
+                  <p className='text-m font-bold'>{message.senderName}</p>
+                  <p>{message.body}</p>
+                  <p className='text-sm opacity-70'>
+                    {message.timestamp
+                      ? new Date(message.timestamp).toLocaleTimeString()
+                      : 'Invalid Time'}
+                  </p>
+                </div>
               </div>
-            </div>
-          </>
-        )}
+            );
+          })}
+        </div>
+
+        {/* Contenedor de input para escribir el mensaje */}
+        <div className='p-4 border-t border-gray-700 bg-black-dark mt-auto'>
+          <div className='flex gap-2'>
+            <input
+              type='text'
+              value={newMessage}
+              onChange={(e) => setNewMessage(e.target.value)}
+              onKeyPress={(e) => e.key === 'Enter' && handleSend()}
+              className='flex-1 bg-gray-700 text-white-basic rounded-lg px-4 py-2'
+              placeholder='Type your message...'
+            />
+            <button
+              onClick={handleSend}
+              className='bg-gold-soft text-black-dark px-4 py-2 rounded-lg hover:bg-gold-dark transition-colors'
+            >
+              Send
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
